9f4dd31a8305bdb8ce5ba63850ae2ee5d54fcdf9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-11-29 03:07:34 +0300

    test from srt method of WScaper

---
+++ server.js
@@ -0,0 +1,10 @@
+import WScraper from "./WScraper/index.js";
+import bodyTest from "./test/testBody.js";
+
+const scraper = new WScraper();
+scraper.fromStr(bodyTest);
+
+const result1 = scraper.getElementsByTagName("li");
+const result2 = scraper.getElementsByClassName("filename");
+console.log(result1);
+console.log(result2);
