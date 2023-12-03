027579dbd135f19e8146b255df19d3dda146f028
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-04 00:42:16 +0300

    add router for /api routes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import getIpAddress from "./utils/IPHandler.js";
+import router from "./router/router.js";
 
 const app = express();
 
 const PORT = 3000;
 const IP_ADDRESS = getIpAddress();
 
+app.use(express.json());
 app.use(express.static("frontend"));
 
-app.get("/hello", (req, res) => {
-  res.json("hello");
-});
+app.use("/api", router);
 
 app.listen(PORT, IP_ADDRESS, () => {
-  console.log(`Server listening on ${IP_ADDRESS} : ${PORT}`);
+  console.log(`Server listening on ${IP_ADDRESS}:${PORT}`);
 });
