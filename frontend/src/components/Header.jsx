4354add0da911363ed63d627deaad6fe23bcf790
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-07 00:13:44 +0300

    add feed options to the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { Separator } from "./Icons/PlayerIcons";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="text-white absolute top-0 z-10 flex items-center w-full p-6">
+      <nav className="flex items-center gap-4 pointer-events-auto absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
+        <a href="#">All</a>
+        <Separator />
+        <a href="#">Favourites</a>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
