40eb89025999978183cd1927278b4e7fa69b9e53
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-06 21:39:49 +0300

    create a videoplayer component

---
+++ frontend/src/components/VideoPlayer.jsx
@@ -0,0 +1,27 @@
+import { useRef, useState } from "react";
+
+const VideoPlayer = ({ id, author, src }) => {
+  const [playing, setPlaying] = useState(false);
+  const video = useRef(null);
+
+  const handlePlay = () => {
+    const { current: videoElement } = video;
+    playing ? videoElement.pause() : videoElement.play();
+    setPlaying(!playing);
+  };
+
+  return (
+    <div className="relative height-[inherit]">
+      <video
+        className="w-full h-full object-contain"
+        src={src}
+        loop
+        controls={false}
+        ref={video}
+        onClick={handlePlay}
+      ></video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
