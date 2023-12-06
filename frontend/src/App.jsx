16353c53bf7b4be6d0222d8f8685a02c228f0733
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-06 21:36:06 +0300

    add a sample header and video feed component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+import VideoFeed from "./components/VideoFeed";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="underline">Hello World</h1>
+    <div className="w-screen h-screen flex flex-col place-content-center overflow-hidden">
+      <Header />
+      <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
+        <VideoFeed />
+      </main>
     </div>
   );
 };
