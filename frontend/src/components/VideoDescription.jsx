197f3eef418705273fe6e338a719cd212ac667e9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-07 23:01:55 +0300

    add icons for folder and video name

--- frontend/src/components/VideoDescription.jsx
+++ frontend/src/components/VideoDescription.jsx
@@ -2,9 +2,9 @@ const VideoDescription = ({ description }) => {
   const videoName = description.src.split("/").pop();
 
   return (
-    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-12 w-full z-10 p-4">
-      <div className="font-bold text-xl">{description.folder}</div>
-      <div>{videoName}</div>
+    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-16 w-full z-10 p-2">
+      <div className="font-bold text-xl">📁 {description.folder}</div>
+      <div>📹 {videoName}</div>
     </div>
   );
 };
