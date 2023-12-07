17be7058cff5de5c84f6d8db7bcec680f4532e2b
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-07 17:05:46 +0300

    adjust the position of video description comp

--- frontend/src/components/VideoDescription.jsx
+++ frontend/src/components/VideoDescription.jsx
@@ -2,7 +2,7 @@ const VideoDescription = ({ description }) => {
   const videoName = description.src.split("/").pop();
 
   return (
-    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-0 w-full z-10 p-4">
+    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-12 w-full z-10 p-4">
       <div className="font-bold text-xl">{description.folder}</div>
       <div>{videoName}</div>
     </div>
