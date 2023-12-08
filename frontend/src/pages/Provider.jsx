e10d3cbd0579d5d89b17d53e2d4759d356704af3
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-09 02:00:02 +0300

    finish add button UI and modal opening functionality

--- frontend/src/pages/Provider.jsx
+++ frontend/src/pages/Provider.jsx
@@ -1,9 +1,29 @@
 import Default from "../layouts/Default";
+import { Add } from "../components/Icons/ProviderIcons";
+import { useState } from "react";
+import AddProviderModal from "../components/AddProviderModal";
 
 const Provider = () => {
+  const [isOpen, setOpen] = useState(false);
+  const handleAddClick = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <Default>
-      <h2>Provider</h2>
+      <div className="flex flex-col h-screen px-4">
+        <main className="flex-grow">main</main>
+        {isOpen && <AddProviderModal onClick={handleAddClick} />}
+        <div className="mb-14 flex">
+          <div className="flex-grow"></div>
+          <button
+            className="px-4 py-4 bg-black rounded-lg mb-4"
+            onClick={handleAddClick}
+          >
+            <Add />
+          </button>
+        </div>
+      </div>
     </Default>
   );
 };
