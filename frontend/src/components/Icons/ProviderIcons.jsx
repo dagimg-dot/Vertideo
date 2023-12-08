a78e8b799e2a3ee6f62e3b1162aa98bf7db1b7f0
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-09 01:59:30 +0300

    create add icon

---
+++ frontend/src/components/Icons/ProviderIcons.jsx
@@ -0,0 +1,15 @@
+const Add = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14 7V8H8V14H7V8H1V7H7V1H8V7H14Z" fill="#C5C5C5" />
+    </svg>
+  );
+};
+
+export { Add };
