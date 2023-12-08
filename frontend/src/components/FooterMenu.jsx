ad14a7034a175b8dc7da65c49caa4c927359790a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-09 00:38:14 +0300

    make the footer menu fixed for all the pages

--- frontend/src/components/FooterMenu.jsx
+++ frontend/src/components/FooterMenu.jsx
@@ -1,22 +1,26 @@
-import { Activity, Home, Profile, Search } from "./Icons/FooterMenuIcons";
+import { Activity, Home, Search } from "./Icons/FooterMenuIcons";
+import { Link, useLocation } from "react-router-dom";
 
 const white = "#fff";
 const FooterMenu = () => {
+  const { pathname } = useLocation();
+
+  const makeSolid = (path) => {
+    return pathname === path;
+  };
+
   return (
-    <footer className="flex items-center justify-between w-full bg-black px-4">
-      <a className="flex items-center justify-center p-2" href="#">
-        <Home color={white} solid={true} />
-      </a>
-      <a className="flex items-center justify-center p-2" href="#">
-        <Search color={white} />
-      </a>
-      <a className="flex items-center justify-center p-2" href="#">
-        <Activity color={white} />
-      </a>
-      <a className="flex items-center justify-center p-2" href="#">
-        <Profile color={white} />
-      </a>
-    </footer>
+    <nav className="flex items-center justify-between w-full bg-black px-4">
+      <Link to="/provider" className="flex items-center justify-center p-2">
+        <Home color={white} solid={makeSolid("/provider")} />
+      </Link>
+      <Link to="/" className="flex items-center justify-center p-2">
+        <Activity color={white} solid={makeSolid("/")} />
+      </Link>
+      <Link to="/search" className="flex items-center justify-center p-2">
+        <Search color={white} solid={makeSolid("/search")} />
+      </Link>
+    </nav>
   );
 };
 
