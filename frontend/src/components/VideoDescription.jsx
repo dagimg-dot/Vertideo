fab0c124aad189f9274f65849298a0bf572f44ed
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-09 00:38:50 +0300

    change position relative to seekbar

--- frontend/src/components/VideoDescription.jsx
+++ frontend/src/components/VideoDescription.jsx
@@ -2,7 +2,7 @@ const VideoDescription = ({ description }) => {
   const videoName = description.src.split("/").pop();
 
   return (
-    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-12 w-full z-10 p-2">
+    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-28 w-full p-2">
       <div className="font-bold text-xl">📁 {description.folder}</div>
       <div>📹 {videoName}</div>
     </div>
