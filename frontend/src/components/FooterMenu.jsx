3330444328aaa8cdc9a8be6277062142c4d82bda
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-09 17:23:57 +0300

    use the refactored icons

--- frontend/src/components/FooterMenu.jsx
+++ frontend/src/components/FooterMenu.jsx
@@ -1,7 +1,6 @@
-import { Activity, Home, Search } from "./Icons/FooterMenuIcons";
+import { FeedIcon, ProviderIcon, SearchIcon } from "./Icons/FooterMenuIcons";
 import { Link, useLocation } from "react-router-dom";
 
-const white = "#fff";
 const FooterMenu = () => {
   const { pathname } = useLocation();
 
@@ -11,14 +10,14 @@ const FooterMenu = () => {
 
   return (
     <nav className="flex items-center justify-between w-full bg-black px-4">
-      <Link to="/provider" className="flex items-center justify-center p-2">
-        <Home color={white} solid={makeSolid("/provider")} />
+      <Link to="/provider" className="flex items-center justify-center p-3">
+        <ProviderIcon color={"white"} solid={makeSolid("/provider")} />
       </Link>
-      <Link to="/" className="flex items-center justify-center p-2">
-        <Activity color={white} solid={makeSolid("/")} />
+      <Link to="/" className="flex items-center justify-center p-3">
+        <FeedIcon color={"white"} solid={makeSolid("/")} />
       </Link>
-      <Link to="/search" className="flex items-center justify-center p-2">
-        <Search color={white} solid={makeSolid("/search")} />
+      <Link to="/search" className="flex items-center justify-center p-3">
+        <SearchIcon color={"white"} solid={makeSolid("/search")} />
       </Link>
     </nav>
   );
