8249a0d2397265c7bd5d1c89941909d99b628057
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-10 22:42:37 +0300

    remove default layout

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,17 +1,16 @@
+import FooterMenu from "../components/FooterMenu";
 import Header from "../components/Header";
 import VideoFeed from "../components/VideoFeed";
-import Default from "../layouts/Default";
 
 const Feed = () => {
   return (
-    <Default>
-      <div className="w-screen h-screen flex flex-col place-content-center overflow-hidden">
-        <Header />
-        <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
-          <VideoFeed />
-        </main>
-      </div>
-    </Default>
+    <div className="w-screen h-screen flex flex-col place-content-center overflow-hidden">
+      <Header />
+      <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
+        <VideoFeed />
+      </main>
+      <FooterMenu />
+    </div>
   );
 };
 
