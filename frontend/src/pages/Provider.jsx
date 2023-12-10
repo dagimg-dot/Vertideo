3b68ead47cf5e14f6a9dcbbbd6ffb5785e9a304c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-11 01:25:02 +0300

    finish styling the provider page and handle no provider error

--- frontend/src/pages/Provider.jsx
+++ frontend/src/pages/Provider.jsx
@@ -2,6 +2,7 @@ import Default from "../layouts/Default";
 import { Add } from "../components/Icons/ProviderIcons";
 import { useState } from "react";
 import AddProviderModal from "../components/AddProviderModal";
+import ProviderCard from "../components/ProviderCard";
 
 const Provider = () => {
   const [isOpen, setOpen] = useState(false);
@@ -9,15 +10,34 @@ const Provider = () => {
     setOpen(!isOpen);
   };
 
+  const providers = [
+    { folderName: "Videos", URL: "192.168.1.5:8081" },
+    { folderName: "Assets", URL: "192.168.1.5:8081" },
+    { folderName: "Gallery", URL: "192.168.1.5:8081" },
+    { folderName: "Trailers", URL: "192.168.1.5:8081" },
+    { folderName: "Snapchat", URL: "192.168.1.5:8081" },
+  ];
+
   return (
     <Default>
       <div className="flex flex-col px-4 h-full">
-        <main className="flex-grow">main</main>
+        <main className="flex-grow">
+          <div className="py-4 text-3xl mb-8">
+            <h1>Providers</h1>
+          </div>
+          <div className="flex flex-col gap-4">
+            {providers.length !== 0 ? (
+              providers.map((provider) => <ProviderCard key={provider.folderName} {...provider} />)
+            ) : (
+              <div>You have no providers !</div>
+            )}
+          </div>
+        </main>
         {isOpen && <AddProviderModal onClick={handleAddClick} />}
         <div className="flex">
           <div className="flex-grow"></div>
           <button
-            className="px-4 py-4 bg-black rounded-lg mb-4"
+            className="px-4 py-4 bg-[#bcfb08] rounded-lg mb-6"
             onClick={() => setOpen(true)}
           >
             <Add />
