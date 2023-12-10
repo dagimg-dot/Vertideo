156a95d147b22f1142ee9f382bfb0c7b712eef22
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-11 01:18:25 +0300

    handle no videos error

--- frontend/src/components/VideoFeed.jsx
+++ frontend/src/components/VideoFeed.jsx
@@ -1,4 +1,5 @@
 import VideoPlayer from "./VideoPlayer";
+import { Link } from "react-router-dom";
 
 const Videos = [
   {
@@ -24,13 +25,24 @@ const Videos = [
 ];
 
 const VideoFeed = () => {
-  return Videos.map((video) => {
-    return (
-      <div key={video.id} className="w-full h-full snap-center scroll-smooth">
-        <VideoPlayer {...video} />
-      </div>
-    );
-  });
+  if (Videos.length !== 0) {
+    return Videos.map((video) => {
+      return (
+        <div key={video.id} className="w-full h-full snap-center scroll-smooth">
+          <VideoPlayer {...video} />
+        </div>
+      );
+    });
+  }
+  return (
+    <div className="absolute left-1/2 -translate-x-1/2 top-20 w-full p-10">
+      You don't have any providers. Go to the{" "}
+      <Link to="/provider" className="text-blue-400">
+        provider
+      </Link>{" "}
+      page and add at least one provider.
+    </div>
+  );
 };
 
 export default VideoFeed;
