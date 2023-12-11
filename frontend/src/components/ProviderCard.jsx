a128721424f6ccb3e54cf33a2303f22b6a743cfd
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-11 16:33:21 +0300

    manipulate data for display

--- frontend/src/components/ProviderCard.jsx
+++ frontend/src/components/ProviderCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const ProviderCard = ({ folderName, URL }) => {
+const ProviderCard = ({ foldername, hostname, port }) => {
   const [isHidden, setIsHidden] = useState(true);
 
   const handleCardClick = () => {
@@ -11,14 +11,23 @@ const ProviderCard = ({ folderName, URL }) => {
     return str.split("").map((s) => "•");
   };
 
+  const constructURL = (hostname, port) => {
+    return hostname + ":" + port;
+  };
+
   return (
     <div
       className="flex justify-between bg-[#181f21] p-4 rounded-lg items-center shadow-md"
       onClick={handleCardClick}
     >
       <div>
-        <h1 className="text-lg font-bold">{folderName}</h1>
-        <span> {isHidden ? getDots(URL) : URL}</span>
+        <h1 className="text-lg font-bold">{foldername}</h1>
+        <span>
+          {" "}
+          {isHidden
+            ? getDots(constructURL(hostname, port))
+            : constructURL(hostname, port)}
+        </span>
       </div>
       <div className="flex gap-2">
         <button>Edit</button>
