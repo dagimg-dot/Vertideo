53d1dc70eb324e197588c125bb7dbf6660ff5d73
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-11 16:31:08 +0300

    wrap all the pages with the global provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import Feed from "./pages/Feed";
 import Provider from "./pages/Provider";
 import Search from "./pages/Search";
+import { GlobalProvider } from "./store/store";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Feed />} />
-      <Route path="/provider" element={<Provider />} />
-      <Route path="/search" element={<Search />} />
-    </Routes>
+    <GlobalProvider>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/provider" element={<Provider />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </GlobalProvider>
   );
 };
 
