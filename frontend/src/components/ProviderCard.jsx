df0d4a223a62c6628a36e2c1d6a8a9ff1354ad7f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-11 15:39:38 +0300

    feat: add toggle to show and hide provider url

--- frontend/src/components/ProviderCard.jsx
+++ frontend/src/components/ProviderCard.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 const ProviderCard = ({ folderName, URL }) => {
+  const [isHidden, setIsHidden] = useState(true);
+
+  const handleCardClick = () => {
+    setIsHidden(!isHidden);
+  };
+
+  const getDots = (str) => {
+    return str.split("").map((s) => "•");
+  };
+
   return (
-    <div className="flex justify-between bg-[#181f21] p-4 rounded-lg items-center shadow-md">
+    <div
+      className="flex justify-between bg-[#181f21] p-4 rounded-lg items-center shadow-md"
+      onClick={handleCardClick}
+    >
       <div>
         <h1 className="text-lg font-bold">{folderName}</h1>
-        <span>{URL}</span>
+        <span> {isHidden ? getDots(URL) : URL}</span>
       </div>
       <div className="flex gap-2">
         <button>Edit</button>
