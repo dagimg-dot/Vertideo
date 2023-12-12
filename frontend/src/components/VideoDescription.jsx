239bccb13be07cac6f83bf8b45b43023cbf253a3
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-12 19:19:46 +0300

    remove unnecessary styles

--- frontend/src/components/VideoDescription.jsx
+++ frontend/src/components/VideoDescription.jsx
@@ -2,7 +2,7 @@ const VideoDescription = ({ description }) => {
   const videoName = description.src.split("/").pop();
 
   return (
-    <div className="text-white text-lg flex flex-col justify-between gap-2 absolute bottom-14 w-full p-2">
+    <div className="text-white text-lg flex flex-col gap-2 absolute bottom-14 p-1">
       <div className="font-bold text-xl">📁 {description.folder}</div>
       <div>📹 {videoName}</div>
     </div>
