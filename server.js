8e33ad0ff37b1f7f37f66fe6985d899c7edc095f
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-13 01:20:33 +0300

    use cors pacakge

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import getIpAddress from "./utils/IPHandler.js";
 import router from "./router/router.js";
+import cors from "cors";
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = 3000;
 const IP_ADDRESS = getIpAddress();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static("frontend"));
 
 app.use("/api", router);
