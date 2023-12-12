29128e5ec9a16f1fcf140f6fb810eb0f5fe622b9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-12 21:51:34 +0300

    change shadow color for modal

--- frontend/src/components/AddProviderModal.jsx
+++ frontend/src/components/AddProviderModal.jsx
@@ -18,8 +18,8 @@ const AddProviderModal = ({ toggleModal }) => {
   };
 
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-[#181f21] rounded-lg text-lg text-[#bbb] shadow-md shadow-[#131313]">
-      <form className="flex flex-col gap-4 mb-4" onSubmit={handleSaveClick}>
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-[#181f21] rounded-lg text-lg text-[#bbb] shadow-lg shadow-black">
+      <form className="flex flex-col gap-4" onSubmit={handleSaveClick}>
         <div>
           <label htmlFor="hostname">Hostname</label>
           <input
