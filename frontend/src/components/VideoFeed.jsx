0f5b189062aa4d0acaa376226fc4fa6fac98eda2
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-15 02:06:23 +0300

    use the new loader component in loading state

--- frontend/src/components/VideoFeed.jsx
+++ frontend/src/components/VideoFeed.jsx
@@ -1,6 +1,7 @@
 import VideoPlayer from "./VideoPlayer";
 import { Link } from "react-router-dom";
 import useVideos from "../hooks/useVideos";
+import Loader from "./Loader";
 
 // Test
 
@@ -28,7 +29,15 @@ import useVideos from "../hooks/useVideos";
 // ];
 
 const VideoFeed = () => {
-  const Videos = useVideos();
+  const [isLoading, error, Videos] = useVideos();
+
+  if (isLoading) {
+    return <Loader message={"Fetching your videos..."} />;
+  }
+
+  if (error !== "") {
+    return <div>{error}</div>;
+  }
 
   if (Videos.length !== 0) {
     return Videos.map((video) => {
@@ -38,16 +47,17 @@ const VideoFeed = () => {
         </div>
       );
     });
+  } else {
+    return (
+      <div className="absolute left-1/2 -translate-x-1/2 top-20 w-full p-10">
+        You don't have any providers. Go to the{" "}
+        <Link to="/provider" className="text-blue-400">
+          provider
+        </Link>{" "}
+        page and add at least one provider.
+      </div>
+    );
   }
-  return (
-    <div className="absolute left-1/2 -translate-x-1/2 top-20 w-full p-10">
-      You don't have any providers. Go to the{" "}
-      <Link to="/provider" className="text-blue-400">
-        provider
-      </Link>{" "}
-      page and add at least one provider.
-    </div>
-  );
 };
 
 export default VideoFeed;
