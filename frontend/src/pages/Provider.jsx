dbf8bbb35164ea69618bdc0505a966c8b276adc0
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-19 15:00:00 +0300

    feat: add settings modal for in app settings

--- frontend/src/pages/Provider.jsx
+++ frontend/src/pages/Provider.jsx
@@ -1,24 +1,30 @@
 import Default from "../layouts/Default";
-import { AddIcon } from "../components/Icons/ProviderIcons";
-import { useContext, useState } from "react";
+import { AddIcon, SettingsIcon } from "../components/Icons/ProviderIcons";
+import { useContext, useEffect, useRef, useState } from "react";
 import AddProviderModal from "../components/AddProviderModal";
 import ProviderCard from "../components/ProviderCard";
 import { GlobalContext } from "../store/store";
+import SettingsModal from "../components/SettingsModal";
 
 const Provider = () => {
-  const [isOpen, setOpen] = useState(false);
-  const toggleModal = () => {
-    setOpen(!isOpen);
-  };
-
   const { providers } = useContext(GlobalContext);
+  const [isAddOpen, setAddOpen] = useState(false);
+  const [isSettingsOpen, setSettingsOpen] = useState(false);
+  const settingBtn = useRef(null);
+
+  const toggleSettingsModal = () => {
+    setSettingsOpen(!isSettingsOpen);
+  };
 
   return (
     <Default>
       <div className="flex flex-col px-4 h-full">
         <main className="flex-grow">
-          <div className="py-4 text-3xl mb-8">
+          <div className="flex justify-between py-4 text-3xl mb-8 items-center">
             <h1>Providers</h1>
+            <div onClick={toggleSettingsModal} ref={settingBtn}>
+              <SettingsIcon />
+            </div>
           </div>
           <div className="flex flex-col gap-4">
             {providers.length !== 0 ? (
@@ -30,12 +36,20 @@ const Provider = () => {
             )}
           </div>
         </main>
-        {isOpen && <AddProviderModal toggleModal={toggleModal} />}
+        {isAddOpen && (
+          <AddProviderModal toggleModal={() => setAddOpen(!isAddOpen)} />
+        )}
+        {isSettingsOpen && (
+          <SettingsModal
+            toggleModal={toggleSettingsModal}
+            btnRef={settingBtn}
+          />
+        )}
         <div className="flex">
           <div className="flex-grow"></div>
           <button
             className="px-4 py-4 bg-[#bcfb08] rounded-lg mb-6"
-            onClick={() => setOpen(true)}
+            onClick={() => setAddOpen(true)}
           >
             <AddIcon />
           </button>
