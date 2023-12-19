ccfa8ef960dc8f51e645a96bf0c6906d4eff97c5
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-19 23:21:10 +0300

    feat: add two settings UI

--- frontend/src/components/SettingsModal.jsx
+++ frontend/src/components/SettingsModal.jsx
@@ -1,4 +1,5 @@
 import useOutsideClick from "../hooks/useOutSideClick";
+import Switch from "./Switch";
 
 const SettingsModal = ({ toggleModal, btnRef }) => {
   const settingRef = useOutsideClick(() => {
@@ -8,9 +9,23 @@ const SettingsModal = ({ toggleModal, btnRef }) => {
   return (
     <div
       ref={settingRef}
-      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-[#181f21] rounded-lg text-lg text-[#bbb] shadow-lg shadow-black"
+      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#181f21] rounded-lg text-lg text-[#bbb] shadow-lg shadow-black w-3/4"
     >
-      hello
+      <div className="flex flex-col p-4">
+        <div className="text-center">
+          <h1 className="font-bold text-2xl">Settings</h1>
+        </div>
+        <div className="flex flex-col gap-4 my-6">
+          <div className="flex justify-between items-center bg-[#101115] p-4 rounded-lg">
+            <span>Repeat video when it ends</span>
+            <Switch />
+          </div>
+          <div className="flex justify-between items-center bg-[#101115] p-4 rounded-lg">
+            <span>Dark Mode</span>
+            <Switch />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
