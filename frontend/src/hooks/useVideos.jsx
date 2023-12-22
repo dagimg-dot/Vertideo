e982b046df4765761e4b2b7c45d3c8974b9be865
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-22 22:56:15 +0300

    refactor: separate video randomizing method to separate file

--- frontend/src/hooks/useVideos.jsx
+++ frontend/src/hooks/useVideos.jsx
@@ -1,57 +1,30 @@
 import { useState, useContext, useEffect } from "react";
 import { GlobalContext } from "../store/store";
-
-const randomizeVideos = (Videos) => {
-  const randomizedVideos = [...Videos];
-  const getRand = () => Math.floor(Math.random() * randomizedVideos.length);
-
-  for (let i = 0; i < randomizedVideos.length; i++) {
-    let rand = getRand();
-    let rand2 = getRand();
-    let temp = randomizedVideos[rand];
-    randomizedVideos[rand] = randomizedVideos[rand2];
-    randomizedVideos[rand2] = temp;
-  }
-
-  return randomizedVideos;
-};
+import randomizeVideos from "../utils/randomizeVideos";
 
 const useVideos = () => {
   const { providers, SaveVideos } = useContext(GlobalContext);
-  const [Videos, setVideos] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const fullUrlConstructor = (provider) => {
     // Test
-    return "http://192.168.1.3:5501/test/";
-    // return (
-    //   "http://" +
-    //   provider.hostname +
-    //   ":" +
-    //   provider.port +
-    //   "/" +
-    //   "share/" +
-    //   provider.foldername
-    // );
-  };
-
-  const getAllVideos = () => {
-    let allVideos = [];
-
-    providers.map((provider) => {
-      if (provider.videos.length > 0) {
-        allVideos = [...allVideos, ...provider.videos];
-      }
-    });
-
-    return allVideos;
+    // return "http://192.168.1.4:5501/test/";
+    return (
+      "http://" +
+      provider.hostname +
+      ":" +
+      provider.port +
+      "/" +
+      "share/" +
+      provider.foldername
+    );
   };
 
   const fetchVideos = async (idx) => {
     setLoading(true);
     try {
-      const res = await fetch("http://192.168.1.3:3000/api/videos", {
+      const res = await fetch("http://192.168.1.4:3000/api/videos", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -59,15 +32,12 @@ const useVideos = () => {
         body: JSON.stringify({ url: fullUrlConstructor(providers[idx]) }),
       });
       const data = await res.json();
-      // console.log(data.data);
 
       if (data.error) {
         throw new Error(data.error.message);
       }
 
       const randomizedVideos = randomizeVideos(data.data);
-      // const randomizedVideos = data.data;
-
       SaveVideos({ id: providers[idx].id, videos: randomizedVideos });
     } catch (error) {
       setError(error.message);
@@ -82,12 +52,10 @@ const useVideos = () => {
       providers.forEach((provider, idx) => {
         fetchVideos(idx);
       });
-
-      setVideos(getAllVideos());
     }
   }, []);
 
-  return [isLoading, error, Videos];
+  return [isLoading, error];
 };
 
 export default useVideos;
