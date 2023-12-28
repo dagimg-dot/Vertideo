bc97132f92b5cf74a998e1904c97d48c65d62ca7
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-28 08:31:14 +0300

    feat: generate thumbnail for each video using ffmpeg

--- service/videoService.js
+++ service/videoService.js
@@ -1,4 +1,7 @@
 import WScraper from "../WScraper/index.js";
+import getIpAddress from "../utils/IPHandler.js";
+import randomIdGenerator from "../utils/randomIdGenerator.js";
+import generateThumbnail from "./thumbnailService.js";
 
 const scraper = new WScraper();
 
@@ -10,20 +13,34 @@ const generateURL = (url, videoNameList) => {
   return result;
 };
 
+const generateThumbnailLink = (videoSrc) => {
+  const IPADDRESS = getIpAddress();
+  const videoName = videoSrc.split("/").pop().split(".")[0];
+  const thumbnailLink = `http://${IPADDRESS}:3000/thumbnails/thumbnail-${videoName}.png`;
+  return thumbnailLink;
+};
+
 const getAllVideos = async (url) => {
   await scraper.fromURL(url);
   const result = scraper.getElementsByClassName("filename");
   const extractedVideoNames = scraper.innerText(result);
   const videoLinks = generateURL(url, extractedVideoNames);
 
-  const videos = videoLinks.map((vid, idx) => {
-    return {
-      id: idx,
-      folder: vid.split("/")[4],
+  for (let i = 0; i < videoLinks.length; i++) {
+    await generateThumbnail(videoLinks[i], "thumbnails");
+  }
+
+  const videos = videoLinks.map((vid) => {
+    const video = {
+      id: randomIdGenerator(),
+      // folder: vid.split("/")[4],
+      folder: vid.split("/")[3], // For testing
+      thumbnail: generateThumbnailLink(vid),
       src: vid,
     };
-  });
 
+    return video;
+  });
   return videos;
 };
 
