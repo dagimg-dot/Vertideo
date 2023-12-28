bf8ef6ccf6d5e02bae124e5a1f5a1e3a8ae650d4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-28 08:33:16 +0300

    feat: show thumbnails for each video

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -7,8 +7,11 @@ const VideoCard = ({ video, token }) => {
 
   return (
     <div className="flex bg-[#181f21] p-3 rounded-lg items-center shadow-md gap-4">
-      {/* This is for video thumbnail*/}
-      <div className=" bg-slate-700 p-4 rounded-md">T</div>
+      <img
+        src={video.thumbnail}
+        alt={video.src}
+        className="w-12 h-12 rounded-md"
+      />
       <div className="flex flex-col">
         <div className="flex">
           <span>{first}</span>
