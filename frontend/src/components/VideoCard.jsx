d512bdbb141e5c6936807b5310c7008fb84e4b4c
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-12-28 14:59:47 +0300

    feat: wrap the div with link to navigate to clicked video

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const VideoCard = ({ video, token }) => {
   const videoName = video.src.split("/").pop();
 
@@ -6,21 +8,23 @@ const VideoCard = ({ video, token }) => {
   const second = videoName.substring(idx + token.length);
 
   return (
-    <div className="flex bg-[#181f21] p-3 rounded-lg items-center shadow-md gap-4">
-      <img
-        src={video.thumbnail}
-        alt={video.src}
-        className="w-12 h-12 rounded-md"
-      />
-      <div className="flex flex-col">
-        <div className="flex">
-          <span>{first}</span>
-          <span className="bg-[#bcfb08] text-[#101115]">{token}</span>
-          <span>{second}</span>
+    <Link key={video.src} to={`/#${video.id}`} className="cursor-pointer">
+      <div className="flex bg-[#181f21] p-3 rounded-lg items-center shadow-md gap-4">
+        <img
+          src={video.thumbnail}
+          alt={video.src}
+          className="w-12 h-12 rounded-md"
+        />
+        <div className="flex flex-col">
+          <div className="flex">
+            <span>{first}</span>
+            <span className="bg-[#bcfb08] text-[#101115]">{token}</span>
+            <span>{second}</span>
+          </div>
+          <span>{video.folder}</span>
         </div>
-        <span>{video.folder}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
