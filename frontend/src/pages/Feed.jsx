509961ddd7e6c268dd490a32914ba2e6ac62c153
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-13 09:59:38 -0800

    change height from screen to 100vdh

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -4,7 +4,7 @@ import VideoFeed from "../components/VideoFeed";
 
 const Feed = () => {
   return (
-    <div className="w-screen h-screen flex flex-col place-content-center overflow-hidden">
+    <div className="w-screen h-[100dvh] flex flex-col place-content-center overflow-hidden">
       <Header />
       <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
         <VideoFeed />
