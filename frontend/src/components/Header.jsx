3e3223e7f037857b2c9f900b37f345df6d32ce34
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-13 12:43:28 -0800

    add event listener to listen onClick event

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import { Separator } from "./Icons/PlayerIcons";
+import { GlobalContext } from "../store/store";
 
 const Header = () => {
+  const {toggleClicked} = useContext(GlobalContext)
+  const handleClick = (event) => {
+    event.target.innerHTML === 'Favorites' ? toggleClicked(true) : toggleClicked(false)
+  }
   return (
     <header className="text-white absolute top-0 z-10 flex items-center w-full p-6 bottom-0 h-8 bg-gradient-to-b from-[#101115] to-transparent">
       <nav className="flex items-center gap-4 pointer-events-auto absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
-        <a href="#">All</a>
+        <a href="#" onClick={(event) => handleClick(event)}>All</a>
         <Separator />
-        <a href="#">Favorites</a>
+        <a href="#" onClick={(event) => handleClick(event)}>Favorites</a>
       </nav>
     </header>
   );
