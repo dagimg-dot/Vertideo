4948abecdd9ff5eaa6de9dc60b73acc6293a2486
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-13 12:44:06 -0800

    toggle between videofeed and favouritefeed component using isFavouriteClicked

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import FooterMenu from "../components/FooterMenu";
 import Header from "../components/Header";
 import VideoFeed from "../components/VideoFeed";
+import { GlobalContext } from "../store/store";
+import FavouriteFeed from "../components/FavouriteFeed";
 
 const Feed = () => {
+  const {isFavouriteClicked} = useContext(GlobalContext)
+  console.log(isFavouriteClicked)
   return (
     <div className="w-screen h-[100dvh] flex flex-col place-content-center overflow-hidden">
       <Header />
       <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
-        <VideoFeed />
+        {isFavouriteClicked ? <FavouriteFeed/> : 
+        <VideoFeed />}
       </main>
       <FooterMenu />
     </div>
