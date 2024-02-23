51093585df397f6ad71e73ff4a3c9bf74763960d
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-23 11:47:50 -0800

    configure tailwind.config.js to use the fill.current property

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class',
   theme: {
+    fill : theme => ({
+      'black' : theme('colors.black'),
+      'white' : theme('colors.white'),
+    }),
     extend: {},
   },
   plugins: [],
