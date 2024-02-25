357ba6e7b2e762aa9766519a65a4b1c054728aae
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-02-25 02:39:39 -0800

    Refactor FooterMenu component

--- frontend/src/components/FooterMenu.jsx
+++ frontend/src/components/FooterMenu.jsx
@@ -11,13 +11,13 @@ const FooterMenu = () => {
   return (
     <nav className="flex items-center justify-between w-full bg-white dark:bg-[#101115]  px-4 border-t-2 border-[#181f21]">
       <Link to="/provider" className="flex items-center justify-center p-3">
-        <ProviderIcon color={"white"} solid={makeSolid("/provider")} />
+        <ProviderIcon solid={makeSolid("/provider")} />
       </Link>
       <Link to="/" className="flex items-center justify-center p-3">
-        <FeedIcon color={"white"} solid={makeSolid("/")} />
+        <FeedIcon solid={makeSolid("/")} />
       </Link>
       <Link to="/search" className="flex items-center justify-center p-3">
-        <SearchIcon color={"white"} solid={makeSolid("/search")} />
+        <SearchIcon solid={makeSolid("/search")} />
       </Link>
     </nav>
   );
