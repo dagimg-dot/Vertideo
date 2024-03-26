056eecc1ebd40cd4f044ff7d801933fe6867c74b
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-03-26 10:58:48 -0700

    add font color for both light and dark mode to main element that wrapped VideoFeed component

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -9,7 +9,7 @@ const Feed = () => {
   return (
     <div className="w-screen h-[100dvh] flex flex-col place-content-center overflow-hidden">
       <Header />
-      <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative">
+      <main className="h-full w-full aspect-[9/16] rounded-tr-md rounded-tl-md overflow-y-scroll snap-y snap-mandatory block relative bg-white dark:bg-[#101115]">
         <VideoFeed />
       </main>
       <FooterMenu />
